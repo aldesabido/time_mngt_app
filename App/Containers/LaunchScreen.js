52ea0f4dd198d9526d24fd2a85d511cf86377877
Dan <danjoaquinchin@gmail.com>
2018-04-14 15:49:31 +0800

    hello world

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -18,7 +18,7 @@ export default class LaunchScreen extends Component {
           </View>
 
           <View style={styles.section} >
-            <Text style={styles.sectionText}> LOG-IN </Text>
+            <Text style={styles.sectionText}> LOG-out </Text>
             <Text style={styles.sectionText}>
               This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
             </Text>
